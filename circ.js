292bffcbcfe5cd38eea35790f11d26d1bd25cdbb
Sofia Rodrigues Ferreira <67131611+SofiaRodFer@users.noreply.github.com>
2020-08-19 10:06:55 -0300

    Add files via upload

--- circ.js
+++ circ.js
@@ -2,15 +2,15 @@ function raiocirc() {
 
     var raio = document.getElementById("raio").value;
     var raiovalor = (raio * raio * 3.14)
-    document.getElementById("respostaraio").innerHTML = "Área: " + raiovalor;
+    document.getElementById("respostaraio").innerHTML = "A área da circunferência de raio = " + raio + " é: " + raiovalor;
 
 }
 
 function volcirc() {
     var raiovol = document.getElementById("raio").value;
     var altura = document.getElementById("altura").value;
-    var volumevalor = (raiovol * raiovol * 3,14 * altura)
-    document.getElementById("respostavolume").innerHTML = "Volume: " + volumevalor;
+    var volumevalor = (raiovol * raiovol * 3.14 * altura)
+    document.getElementById("respostavolume").innerHTML = "O volume do cilíndro com base de área = " + (raiovol * raiovol * 3.14) + " e altura = " + altura + " é: " + volumevalor;
 
 }
 
